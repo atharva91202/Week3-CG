501aead79b987c59d2fcd12da1548f58e2d6106b
Atharva <atharvasharma.cs25@gmail.com>
2025-01-30 16:33:46 +0530

    Task Schedular added

---
+++ src/problemstatements/circularlinkedlist/taskschedulling/Task.java
@@ -0,0 +1,17 @@
+package problemstatements.circularlinkedlist.taskschedulling;
+
+class Task {
+    int taskID;
+    String taskName;
+    int priority;
+    String dueDate;
+    Task next;
+
+    public Task(int taskID, String taskName, int priority, String dueDate) {
+        this.taskID = taskID;
+        this.taskName = taskName;
+        this.priority = priority;
+        this.dueDate = dueDate;
+        this.next = null;
+    }
+}
