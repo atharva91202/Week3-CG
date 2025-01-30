c7cea77d159aaa76ae292b3a193d332e588ca5c7
Atharva <atharvasharma.cs25@gmail.com>
2025-01-30 16:20:24 +0530

    Text Editor UndoRedo added

---
+++ src/problemstatements/doublylinkedlist/undoredo/TextState.java
@@ -0,0 +1,14 @@
+package problemstatements.doublylinkedlist.undoredo;
+
+
+class TextState {
+    String textContent;
+    TextState next;
+    TextState prev;
+
+    public TextState(String textContent) {
+        this.textContent = textContent;
+        this.next = null;
+        this.prev = null;
+    }
+}
