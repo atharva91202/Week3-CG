4da0bceed3a1e015eef0403291b649a0edbc56fe
Atharva <atharvasharma.cs25@gmail.com>
2025-02-05 11:24:11 +0530

    [Atharva] Add . Point of Rotation

---
+++ src/binarysearch/findrotationpoint/rotationPoint.java
@@ -0,0 +1,21 @@
+package binarysearch.findrotationpoint;
+
+public class rotationPoint {
+    public static int findRotationPoint(int arr[]){
+        int left = 0,right = arr.length-1;
+        while (left<right){
+            int mid = left + (right-left)/2;
+            if (arr[mid] > arr[right]){
+                left = mid +1;
+            }else{
+                right = mid;
+            }
+        }
+        return left;
+    }
+    public static void main(String[] args) {
+     int [] arr ={4,5,6,7,0,1,2};
+     int index = findRotationPoint(arr);
+        System.out.println("Index of rotation is " + index);
+    }
+}
